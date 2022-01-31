a780439347642421033c87325c1a5f796ef3553f
MicheleRago <michele@rago.ovh>
2022-01-31 03:07:56 +0100

    22

--- kubernetes/user/user_microservice/user.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE utente (
-    id bigint NOT NULL,
-    is_admin bit NOT NULL,
-    mail varchar(255) NOT NULL,
-    password varchar(255) NOT NULL,
-    PRIMARY KEY (id),
-     UNIQUE (mail)
-);
